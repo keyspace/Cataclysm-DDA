e806e2a257c8f6c03d7aa051319b37143da0506c
keyspace <keyspace@users.noreply.github.com>
2017-01-13 17:36:36 +0200

    vehicle/collision/flowers: add "special parts" if-clause skeleton

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -4350,18 +4350,25 @@ veh_collision vehicle::part_collision( int part, const tripoint &p,
     } else if( ( bash_floor && g->m.is_bashable_ter_furn( p, true ) ) ||
                // Movecost 2 indicates flat terrain like a floor, no collision there.
                ( g->m.is_bashable_ter_furn( p, false ) && g->m.move_cost_ter_furn( p ) != 2 &&
-                // Don't collide with tiny things, like flowers, unless we have a wheel in our space.
-                (part_with_feature(ret.part, VPFLAG_WHEEL) >= 0 ||
-                 !g->m.has_flag_ter_or_furn("TINY", p)) &&
-                // Protrusions don't collide with short terrain.
-                // Tiny also doesn't, but it's already excluded unless there's a wheel present.
-                !(part_with_feature(ret.part, "PROTRUSION") >= 0 &&
-                  g->m.has_flag_ter_or_furn("SHORT", p)) &&
-                // These are bashable, but don't interact with vehicles.
-                !g->m.has_flag_ter_or_furn("NOCOLLIDE", p) ) ) {
+                 // These are bashable, but don't interact with vehicles - exclude them.
+                 !g->m.has_flag_ter_or_furn( "NOCOLLIDE", p ) ) ) {
         ret.type = veh_coll_bashable;
         terrain_collision_data( p, bash_floor, mass2, part_dens, e );
         ret.target_name = g->m.disp_name( p );
+    } else if( g->m.is_bashable_ter_furn( p, false ) ) {
+        // Check special parts that collide even on "flat terrain".
+        // Tiny things, like flowers, collide with wheels. // TODO: check VPFLAG_TRACK
+        // Don't have to check short here: wheels need frames, those collide with everything.
+        const int maybe_wheel_part = part_with_feature( ret.part, VPFLAG_WHEEL );
+        if( maybe_wheel_part >= 0 && g->m.has_flag_ter_or_furn( "TINY", p ) ) {
+            // TODO
+        }
+        // Protrusions don't collide with tiny/short furniture, they do otherwise.
+        const int maybe_protruding_part = part_with_feature( ret.part, "PROTRUSION" );
+        if( maybe_protruding_part >= 0 && !( g->m.has_flag_ter_or_furn( "TINY", p ) ||
+                                             g->m.has_flag_ter_or_furn( "SHORT", p ) ) ) {
+            // TODO
+        }
     } else if( g->m.impassable_ter_furn( p ) ||
                ( bash_floor && !g->m.has_flag( TFLAG_NO_FLOOR, p ) ) ) {
         ret.type = veh_coll_other; // not destructible
