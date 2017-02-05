aa61b3234d259f75bec76e110f808b49666d2d36
keyspace <keyspace@users.noreply.github.com>
2017-02-05 23:18:29 +0200

    skill: save overflowed excercise when increasing level.

--- src/skill.cpp
+++ src/skill.cpp
@@ -163,8 +163,9 @@ void SkillLevel::train(int amount, bool skip_scaling)
         }
     }
 
-    if( _exercise >= 100 * (_level + 1) * (_level + 1) ) {
-        _exercise = 0;
+    const int overflow = _exercise - 100 * (_level + 1) * (_level + 1);
+    if( overflow >= 0 ) {
+        _exercise = overflow;
         ++_level;
     }
 }
